218087aa359c2caeb7792f24793318827766bf29
gponce84 <48931431+gponce84@users.noreply.github.com>
2019-04-24 23:51:07 -0300

    Rename 01.datosAltaServicios.slq.sql to 01.datosAltaServicios.slq
    cambio de nombre al archivo

--- Test/01.datosAltaServicios.slq.sql
+++
@@ -1,9 +0,0 @@
------------------------------------------
-------ALTAS DE CARGA SERVICIOS---------
-DECLARE @nombre varchar(40)
-exec altaServicio @nombre= 'Pediatría'
-exec altaServicio @nombre= 'Cardiología'
-exec altaServicio @nombre= 'Neumología'
-exec altaServicio @nombre= 'Nutriología'
-exec altaServicio @nombre= 'Oftalmología'
-exec altaServicio @nombre= 'Alergología'
