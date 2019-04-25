e59c7c332a6772923042403b7d8c84efb0467479
gponce84 <48931431+gponce84@users.noreply.github.com>
2019-04-24 23:47:36 -0300

    Update and rename 06.datosBajaLogicaProfecional.slq.sql to 06.datosBajaLogicaProfesional.slq.sql
    Modificación variable profesional

--- Test/06.datosBajaLogicaProfecional.slq.sql
+++
@@ -1,5 +0,0 @@
-------BAJA ALTA LOGICA PROFECIONAL-----
-DECLARE @id int
-EXEC bajaProfecional @id = 1
-
-select * from Profecional
---
+++ Test/06.datosBajaLogicaProfesional.slq.sql
@@ -0,0 +1,5 @@
+------BAJA ALTA LOGICA PROFESIONAL-----
+DECLARE @id int
+EXEC bajaProfesional @id = 1
+
+select * from Profesional
