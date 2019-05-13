ec86b7abf4d198f96cb3edf6b2e319d9808d061f
Ponce Gerardo Adrian <clawgponce@yahoo.com.ar>
2019-05-13 00:37:13 -0300

    Este archivo contiene los Insert a la tabla usuario. Prueba Insert en tabla usuario y Tabla Auditoria

---
+++ Test/10.insertUsuarioFase2.sql
@@ -0,0 +1,4 @@
+insert into	usuario(id, cantidad_por_pagina, nro_pagina) values (1, 10 , 30)
+insert into	usuario(id, cantidad_por_pagina, nro_pagina) values (2, 5 , 15)
+insert into	usuario(id, cantidad_por_pagina, nro_pagina) values (3, 8 , 25)
+insert into	usuario(id, cantidad_por_pagina, nro_pagina) values (20, 8 , 25)
